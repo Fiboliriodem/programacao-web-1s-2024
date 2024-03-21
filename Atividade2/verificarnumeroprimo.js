0cba1b04ea27fe485d8c21d6770543dbcabad910
Fiboliriodem <101954899+Fiboliriodem@users.noreply.github.com>
2024-03-20 23:33:18 -0300

    esqueleto

---
+++ Atividade2/verificarnumeroprimo.js
@@ -0,0 +1,13 @@
+
+function verificarNumeroprimo (n) {
+let isTrue = false
+if ((n%2 !== 0) && (n%3 !== 0)){
+    isTrue = true
+console.log("é primo")
+else {
+    console.log("não é primo")
+}
+return
+}
+
+}
